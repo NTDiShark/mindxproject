0aea5244416a353df9b5c24ce5f84aa4b16985f6
HaoDuong1994 <hao.dv5370@gmail.com>
2023-04-08 05:08:21 +0700

    done modal box

--- banhang.js
+++ banhang.js
@@ -1,13 +1,20 @@
-const $signUp= document.getElementById('btn-sign-up');
-console.log($signUp)
-const modalBox1= document.getElementById('modal-container')
-console.log(modalBox1);
-$signUp.addEventListener('click',()=>{
-    modalBox1.classList.add('show');
-})
 
-const buttonClose= document.getElementById('button-close')
-console.log(buttonClose)
-buttonClose.addEventListener('click',()=>{
-    modalBox1.classList.remove('show');
+const $loginModal=document.getElementById('modal-login-container');
+const $buttonLogin=document.getElementById('button-login');
+$buttonLogin.addEventListener('click',()=>{
+    $loginModal.classList.add('showing');
 });
+const $buttonCloseLogin=document.getElementById('button-login-close');
+$buttonCloseLogin.addEventListener('click',()=>{
+    $loginModal.classList.remove('showing');
+});
+
+const $signupModal=document.getElementById('modal-signup-container');
+const $buttonSignup=document.getElementById('btn-sign-up');
+$buttonSignup.addEventListener('click',()=>{
+    $signupModal.classList.add('show');
+});
+const $buttonCloseSignup=document.getElementById('button-signup-close');
+$buttonCloseSignup.addEventListener('click',()=>{
+    $signupModal.classList.remove('show');
+})
