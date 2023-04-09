6d227e6ed6d45eccd74ca1a69406a0ab7d6cdb17
HaoDuong1994 <hao.dv5370@gmail.com>
2023-04-09 15:38:55 +0700

    edit product data

--- banhang.js
+++ banhang.js
@@ -22,9 +22,9 @@ $buttonCloseSignup.addEventListener('click',()=>{
 const dataPants=[
     {
         idProduct: 1,
-        productName:'Quần tây dài',
-        image:'https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b',
-        price: '5.000.000đ',
+        productName:'Quần Hiddy',
+        image:'https://bizweb.dktcdn.net/100/399/392/files/ten-goi-cac-loai-quan-nam.jpg?v=1657302603507',
+        price: '255.000đ',
         idType:'Quần',
 
     },
@@ -32,21 +32,21 @@ const dataPants=[
         idProduct: 2,
         productName:'Quần tây dài',
         image:'https://product.hstatic.net/1000096703/product/1_97e42ec95db74e9fa48e57a068f88165_master.jpg',
-        price: '5.000.000đ',
+        price: '5.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 3,
-        productName:'Quần tây dài',
-        image:'https://product.hstatic.net/1000096703/product/1_97e42ec95db74e9fa48e57a068f88165_master.jpg',
-        price: '5.000.000đ',
+        productName:'Quần Jean rách',
+        image:'https://thoitrangngaynay.com/upload/sanpham/large/quan-jeans-nam-rach-loang-mau-thoi-trang-488-1.jpg',
+        price: '350.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 4,
-        productName:'Quần tây dài',
+        productName:'Quần Jean xanh',
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMOBnaXvwJvDavW-NWZzH0RZkoyHTphpmn5A&usqp=CAU',
         price: '5.000.000đ',
         idType:'Quần',
@@ -54,65 +54,65 @@ const dataPants=[
     },
     {
         idProduct: 5,
-        productName:'Quần tây dài',
+        productName:'Quần tây xám',
         image:'https://menfit.vn/uploads/m/a/9/U/-/I4PFf_quan-tay-nam-9065-cuongstore.jpg',
-        price: '5.000.000đ',
+        price: '500.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 6,
-        productName:'Quần tây dài',
+        productName:'Quần tây đen',
         image:'https://img.cdn.vncdn.io/cdn-pos/492284-9176/ps/20210206_fSWZ7VWFrwRkPC6GjvawOw7M.jpg',
-        price: '5.000.000đ',
+        price: '500.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 7,
-        productName:'Quần tây dài',
-        image:'https://dony.vn/wp-content/uploads/2022/03/quan-cau-long-nam-dep-ban-chay-1.jpg',
-        price: '5.000.000đ',
+        productName:'Quần Jean trắng',
+        image:'https://thoitrangngaynay.com/upload/sanpham/large/quan-jeans-nam-thiet-ke-o-mau-bam-lo-thoi-trang-801-1.jpg',
+        price: '200.000',
         idType:'Quần',
 
     },
     {
         idProduct: 8,
-        productName:'Quần tây dài',
+        productName:'Quần Kaki',
         image:'https://www.mendaily.vn/images/thumbs/2020/11/quan-kaki-tron-can-ban-qk004-mau-den-15675-slide-products-5f9fd1219ab12.png',
-        price: '5.000.000đ',
+        price: '400.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 9,
-        productName:'Quần tây dài',
-        image:'https://www.mendaily.vn/images/thumbs/2020/11/quan-kaki-tron-can-ban-qk004-mau-den-15675-slide-products-5f9fd1219ab12.png',
-        price: '5.000.000đ',
+        productName:'Quần thun đen',
+        image:'https://lzd-img-global.slatic.net/g/shop/0502b57e666ecb076b30e2438b6cb390.png_1200x1200q80.png_.webp',
+        price: '200.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 10,
-        productName:'Quần tây dài',
-        image:'https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b',
-        price: '5.000.000đ',
+        productName:'Quần short jean',
+        image:'https://menfit.vn/uploads/m/8/O/m/i/PdX1q_thoi-trang-nam-2000-quan-jeans-lung.jpg',
+        price: '300.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 11,
-        productName:'Quần tây dài',
-        image:'https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b',
-        price: '5.000.000đ',
+        productName:'Quần Jean kem',
+        image:'https://vn-live-01.slatic.net/original/c504268d9247efe88483c63a3a772348.jpg',
+        price: '500.000đ',
         idType:'Quần',
 
     },
     {
         idProduct: 12,
-        productName:'Quần tây dài',
-        image:'https://cf.shopee.vn/file/1895d6c9310fb46e43db0e39516e884b',
-        price: '5.000.000đ',
+        productName:'Quần tây kiểu',
+        image:'https://down-vn.img.susercontent.com/file/ed5eed48abd4f589078c73bf5a076a6c_tn',
+        price: '500.000đ',
         idType:'Quần',
 
     },
@@ -121,21 +121,6 @@ console.log(dataPants);
 const $productContainer= document.getElementsByClassName('col-3');
 console.log($productContainer);
 
-const test=[`
-âlka
-sdkldjskldj
-fgg`,`dsklfjdsklfj
-sdfljl
-thu 3`,`thu 4 thu 5`];
-
-const test2=[`
-thu 2
-thu 3
- thu 4
- `];
- console.log(test)
-test.join()
-console.log(test.join(''))
 
 const $pantList=document.getElementById('pantsList')
 console.log($pantList);
