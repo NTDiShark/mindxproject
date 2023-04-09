9dfaaa40f67b9032c470ef164dbe096bd1867be2
kiet.nguyen2@mservice.com.vn <kiet.nguyen2@mservice.com.vn>
2023-04-09 12:46:18 +0700

    init code

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+const container = document.getElementById('container');
 
-const quan = "Day la quan";
-const ao = "Day la ao";
-const dep = "Day la dep";
-function changeContainer() {
-    url = window.location.href;
-    type = url.split("#")[1].split("=")[1];
-    if(type === "quan") document.getElementById("container").innerHTML = quan;
-    if(type === "ao") document.getElementById("container").innerHTML = ao;
-    if(type === "dep") document.getElementById("container").innerHTML = dep;
+const TYPES = [
+    {
+        id: 'quan',
+        name: 'Quần'
+    },
+    {
+        id: 'ao',
+        name: 'Áo'
+    },
+]
 
+const renderFilterMenu = (menu) => {
+    const menuItems = menu.map(item => {
+        return `
+            <span class="box" id="${item.id}"}>${item.name}</span>
+        `;
+    }).join('');
+
+    const _menu = document.getElementById('menu-wrapper');
+    _menu.innerHTML = menuItems;
+
+    menu.forEach(item => {
+        document.getElementById(item.id).addEventListener('click', () => {
+            container.innerHTML = `Đây là những sản phẩm thuộc loại ${item.name}`;
+        });
+    });
 }
+
+renderFilterMenu(TYPES);
