9022f8a43589ffd8d1ea5d4db787e655d6a481d0
NTDiShark <datnguyen2298@gmail.com>
2023-04-15 19:57:23 +0700

    edited

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const container = document.getElementById('container');
+
+const $loginModal=document.getElementById('modal-login-container');
+const $buttonLogin=document.getElementById('button-login');
+$buttonLogin.addEventListener('click',()=>{
+    $loginModal.classList.add('showing');
+});
+const $buttonCloseLogin=document.getElementById('button-login-close');
+$buttonCloseLogin.addEventListener('click',()=>{
+    $loginModal.classList.remove('showing');
+});
+
+const $signupModal=document.getElementById('modal-signup-container');
+const $buttonSignup=document.getElementById('btn-sign-up');
+$buttonSignup.addEventListener('click',()=>{
+    $signupModal.classList.add('show');
+});
+const $buttonCloseSignup=document.getElementById('button-signup-close');
+$buttonCloseSignup.addEventListener('click',()=>{
+    $signupModal.classList.remove('show');
+});
 
 const TYPES = [
     {
@@ -9,23 +28,33 @@ const TYPES = [
         id: 'ao',
         name: 'Áo'
     },
-]
+    {
+        id: 'giay',
+        name: 'Giày'
+    }
+];
+
+const _container = document.getElementById("product-list");
 
 const renderFilterMenu = (menu) => {
     const menuItems = menu.map(item => {
         return `
-            <span class="box" id="${item.id}"}>${item.name}</span>
+            <div class="menu-item" id="${item.id}">${item.name}</div>
         `;
     }).join('');
-
-    const _menu = document.getElementById('menu-wrapper');
+    const _menu = document.getElementById('menu');
+    console.log(menuItems)
     _menu.innerHTML = menuItems;
 
-    menu.forEach(item => {
-        document.getElementById(item.id).addEventListener('click', () => {
-            container.innerHTML = `Đây là những sản phẩm thuộc loại ${item.name}`;
+    menu.forEach(typeProduct => {
+        document.getElementById(typeProduct.id).addEventListener('click', () => {
+            
+            const _product = 
+            _container.innerHTML = `Đây là những sản phẩm thuộc loại ${item.name}`;
         });
     });
 }
 
 renderFilterMenu(TYPES);
+
+
