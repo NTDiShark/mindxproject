f3dd868a555c9cf146bb0920ece561a53b571269
HaoDuong1994 <hao.dv5370@gmail.com>
2023-04-16 19:42:05 +0700

    edit modal

--- modal.js
+++ modal.js
@@ -1,22 +1,18 @@
-const buttonLogin=document.getElementById('button')
-const modalLogin=document.getElementById('id01')
+const buttonLogin=document.getElementById('button');
+const modalLogin=document.getElementById('id01');
 buttonLogin.addEventListener('click',()=>{
     modalLogin.classList.add('show');
 })
 
 window.onclick= function(e){
-    if(e.target==modalLogin){
-        modalLogin.style.display='none'
+    console.log(e.target);
+    if(e.target==modalLogin || e.target == modalSignup){
+        modalLogin.style.display='none';
     }
 };
-const buttonSignup=document.getElementById('button-Signup')
-const modalSignup=document.getElementById('id02')
+const buttonSignup=document.getElementById('button-Signup');
+const modalSignup=document.getElementById('id02');
 buttonSignup.addEventListener('click',()=>{
     modalSignup.classList.add('show');
 });
-window.onclick= function(e){
-    console.log(e.target)
-    if(e.target==modalSignup){
-        modalSignup.style.display='none'
-    }
-};
+
